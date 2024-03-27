3d2f54dda78d26ca3df4bbdd4e1c86eebbf2a242
Katja Toivola <ktoivola@c2r3p11.hive.fi>
2024-03-27 14:17:43 +0200

    adding optimisation

--- push_swap.h
+++ push_swap.h
@@ -30,47 +30,46 @@ typedef struct s_stack_node
 
 int main(int argc, char **argv);
 
-/* Put numbers to stack */
+/* Initialise functions */
 void    init_stack(t_stack_node **a, t_stack_node **b, char **argv, int argc);
 void    append_node(t_stack_node **a, int n);
+int     calculate_segment_size(t_stack_node **a);
 
 /* Free memory and error handling */
 void    free_list(char **ptr);
 void    free_stack(t_stack_node **stack);
 void    handle_error(t_stack_node **a,char **argv, int argc);
 
-
 /* Push_swap sort algorithms */
-void    push_swap(t_stack_node **a, t_stack_node **b);
 void    sort_ministack_3(t_stack_node **stack);
 void    sort_ministack_4(t_stack_node **a, t_stack_node **b);
 void    sort_ministack_5(t_stack_node **a, t_stack_node **b);
+void    push_swap(t_stack_node **a, t_stack_node **b);
 
 /* Push_swap utilities */
 bool            is_sorted(t_stack_node *stack);
+int             stack_len(t_stack_node *stack);
+t_stack_node    *find_last(t_stack_node *node);
 t_stack_node    *find_max(t_stack_node *stack);
 t_stack_node    *find_min(t_stack_node *stack, bool for_index);
-t_stack_node    *find_last(t_stack_node *node);
-int             stack_len(t_stack_node *stack);
 void            do_twice(void (*f)(t_stack_node **a, int i), t_stack_node **s, int x);
 
 
-/* commands */
-void    pa(t_stack_node **a, t_stack_node **b, int i);
-void    pb(t_stack_node **b, t_stack_node **a, int i);
+/* stack commands */
 void    sa(t_stack_node **a, int i);
 void    sb(t_stack_node **b, int i);
-void    ss(t_stack_node **a, t_stack_node **b, int i);
 void    ra(t_stack_node **a, int i);
 void    rb(t_stack_node **b, int i);
-void    rr(t_stack_node **a, t_stack_node **b, int i);
 void    rra(t_stack_node **a, int i);
 void    rrb(t_stack_node **b, int i);
+void    pa(t_stack_node **a, t_stack_node **b, int i);
+void    pb(t_stack_node **b, t_stack_node **a, int i);
+void    ss(t_stack_node **a, t_stack_node **b, int i);
+void    rr(t_stack_node **a, t_stack_node **b, int i);
 void    rrr(t_stack_node **a, t_stack_node **b, int i);
 
 
-void    print_stack(t_stack_node **stack);
+void    print_stack(t_stack_node **stack); // REMOVE before evaluation!!! just for testing
 
-int    calculate_segment_size(t_stack_node **a);
 
 #endif
